b2ad1dbed0db5271aac694913f46fcf7d11b5147
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-02-09 18:23:52 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from 'react'
+
+function App() {
+  const [count,setCount] = useState(0);
+  return (
+    <>
+    <p>You have clicket {count} times</p>
+    <button onClick={()=>
+    {setCount(count+1);}}>Click</button>
+    </>
+    
+    
+  );
+}
+
+export default App;
