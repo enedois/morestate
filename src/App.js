055457d9a4ebf89724d9f99003f4a19064d29434
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-02-09 18:54:19 +1000

    Update App.js
    document title update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react'
 
 function App() {
   const [count,setCount] = useState(0);
+  useEffect(()=> {
+    document.title = `Click x ${count}`;
+  })
   return (
     <>
     <p>You have clicket {count} times</p>
